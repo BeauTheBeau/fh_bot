e25746a19d8ee2a573bfaaf22d0bb21e587436dd
BeauTheBeau <beauthebeau@protonmail.me>
2023-05-28 03:17:05 +0100

    Added client login and added dotenv dependency

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const {Client, GatewayIntentBits} = require('discord.js')
-const fs = require('fs') 
+require('dotenv').config()
 
-const client = new Client({ 
-  intents: 
-    [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildIntegrations,
-      GatewayIntentBits.GuildMessageReactions,     
-      GatewayIntentBits.GuildMessages,          
-      GatewayIntentBits.GuildPresences,
-    ] 
+const {Client, GatewayIntentBits} = require('discord.js')
+const fs = require('fs')
+const token = process.env.TOKEN
+
+const client = new Client({
+    intents:
+        [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildIntegrations,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildPresences,
+        ]
 });
+
 client.commands = new Collection();
 
 const functionFolders = fs.readdirSync(`./functions`);
 for (const folder of functionFolders) {
-  const functionFolders = fs
-    .readdirSync(`./functions/${folder}`)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of functionFolders)
-    require(`./functions/${folder}/${file}`)(client);
+    const functionFolders = fs
+        .readdirSync(`./functions/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of functionFolders)
+        require(`./functions/${folder}/${file}`)(client);
 }
 
-//.env has not been created for token yet
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(token)
