8350a449268cf655f3c4d645c8b9cccea607d3bb
Arnav Mishra <91401687+ArnavDevelops@users.noreply.github.com>
2023-05-28 07:35:33 +0530

    Update index.js
    Added Intents

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const {Client, GatewayIntentBits} = require('discord.js')
 
+const client = new Client({ 
+  intents: 
+    [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildIntegrations,
+      GatewayIntentBits.GuildMessageReactions,     
+      GatewayIntentBits.GuildMessages,          
+      GatewayIntentBits.GuildPresences,
+    ] 
+});
+
+//.env has not been created for token yet
