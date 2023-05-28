d3013affc62cdd3a8f30c390fd950ac2054d75f5
BeauTheBeau <beauthebeau@protonmail.me>
2023-05-28 03:29:12 +0100

    Check for .env
    Signed-off-by: BeauTheBeau <beauthebeau@protonmail.me>

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
 
-const {Client, GatewayIntentBits} = require('discord.js')
+const {Client, GatewayIntentBits, Collection} = require('discord.js')
 const fs = require('fs')
 const token = process.env.TOKEN
-
 const client = new Client({
     intents:
         [
@@ -12,11 +11,18 @@ const client = new Client({
             GatewayIntentBits.GuildIntegrations,
             GatewayIntentBits.GuildMessageReactions,
             GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildPresences,
+            GatewayIntentBits.GuildPresences
         ]
 });
-
 client.commands = new Collection();
+
+// Check if .txt exists
+if (!fs.existsSync('.env')) {
+    console.log('No .env file found, creating one')
+    fs.writeFileSync('.env', `TOKEN=""\nMONGO_URI=""`)
+    console.log('Created .env file, please fill in the token and Mongo URI. Make sure not to commit the .env file to VCS')
+    process.exit()
+}
 
 const functionFolders = fs.readdirSync(`./functions`);
 for (const folder of functionFolders) {
@@ -26,7 +32,6 @@ for (const folder of functionFolders) {
     for (const file of functionFolders)
         require(`./functions/${folder}/${file}`)(client);
 }
-
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
